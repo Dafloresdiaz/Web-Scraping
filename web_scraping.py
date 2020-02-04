c192224fe31f660109c2430d4b57174d945e7401
Dafloresdiaz <dafloresdiaz@gmail.com>
2020-02-03 19:07:17 -0600

    Get the content from the page
    Get the content from the page, solve the issue with the problem 400, it was necessary to put some headers. I need to do more testing on this, to be sure i get the correct content from the page

---
+++ web_scraping.py
@@ -0,0 +1,14 @@
+#The purpose of this function is to obtain the information of HTML page, in order to get the data for the database or csv file; the info is only
+#for Pumas, in the future it will contain more data and maybe other teams.
+#It will obtain the data from this page: https://www.transfermarkt.com/unam-pumas/spielplan/verein/7633/saison_id/2019/plus/1#MEX1
+#The data to obta
+
+import requests
+import urllib.request
+from bs4 import BeautifulSoup as BS
+
+#Make a get request for the URL.
+url = 'https://www.transfermarkt.com/unam-pumas/spielplan/verein/7633/plus/1?saison_id=2008'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+page = requests.get(url, headers = headers)
+print (page)
