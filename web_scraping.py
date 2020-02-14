5992d5f1ce3d21c4030bdadf1b53242b241faaa3
Dafloresdiaz <dafloresdiaz@gmail.com>
2020-02-13 23:36:30 -0600

    Obtain the Time
    Add the code to obtain the time, notice that is better to have a local html file to work, so we can do unlimited request without worry about a ban from the page

--- web_scraping.py
+++ web_scraping.py
@@ -19,10 +19,24 @@ results = soup_Parser.find_all('div', class_='responsive-table')
 
 #Created a HTML file with the results from the page. I suggest to change this functionality to another file to have this functionality separete from the
 #principal objetive of this.
+# In the future i think is important to have this function and work with a local file for the page, in order to avoid a lot of request to the page, the page should be
+# only request one time so i think a good idea is to save onle the necessary information in a html file local and with this work.  THE POINT IS NOT TRYING TO CALL
+# A LOT OF TIMES THE SAME PAGE, WE CAN BE BANNED FOR DO A OF REQUESTS.
+
 create_file = open('Results_Page_Content.html', 'w')
 create_file.write(str(results))
 create_file.close()
 
+
+#The table can be read it by manual calculation, if you go to the table.html, the td where the data is storage the posistion is updated by 7
+#With this code you can obtain the date of the game, the range is the next one 1..8..15 etc
 table_1 = results[0].find_all('td', class_='zentriert')
 text = table_1[1].getText()
+
+#With this code we need to obtain the time
+table_2 = results[0].find_all('td', class_='zentriert')
+text = table_2[9].getText()
+#With this code we can obtain the Home team
+table_3 = results[0].find_all('td', class_='zentriert')
+text = table_3[3].getText()
 print(text)
