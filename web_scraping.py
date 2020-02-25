81fefe8fea540442fc04b4303590e96ba3b57e2d
Dafloresdiaz <dafloresdiaz@gmail.com>
2020-02-24 23:17:42 -0600

    Create HTML Pages
    Modify the code to make a loop to every page for the request page. The result is to obtain all HTML pages for the last 10 years.

--- web_scraping.py
+++ web_scraping.py
@@ -8,38 +8,17 @@ import urllib.request
 from bs4 import BeautifulSoup as BS
 from obtain_html_pages import obtain_html_pages_info as obt
 
-#Make the call to the function to make the request to the url(s) and generate the HTML files.
-html_Content = obt()
+#This for will obtain the information for every season of pumas, from 2010 to 2020, 
+for i in range(2010, 2021):
+    #Make the call to the function to make the request to the url(s) and generate the HTML files.
+    html_Content = obt()
+    #Find the element for the table, it would be the DIV element
+    #TODO: Make the function get_Content() has an attribute to send the year and concatane to the url
+    results = html_Content.get_Content(str(i)).find_all('div', class_='responsive-table')
+    #Created a HTML file with the results from the page. I suggest to change this functionality to another file to have this functionality separete from the
+    #principal objetive of this.
+    create_file = open('Results_Page_Content'+ str(i) + '.html', 'w')
+    create_file.write(str(results))
+    create_file.close()
 
-#Find the element for the table, it would be the DIV element
-results = html_Content.get_Content().find_all('div', class_='responsive-table')
 
-#Created a HTML file with the results from the page. I suggest to change this functionality to another file to have this functionality separete from the
-#principal objetive of this.
-# ! In the future i think is important to have this function and work with a local file for the page, in order to avoid a lot of request to the page, the page should be
-# ! only request one time so i think a good idea is to save onle the necessary information in a html file local and with this work.  THE POINT IS NOT TRYING TO CALL
-# ! A LOT OF TIMES THE SAME PAGE, WE CAN BE BANNED FOR DO A OF REQUESTS!!!.
-# TODO: Create a function to obtain the pages in html format so I can work locally. Read the comment above.
-
-create_file = open('Results_Page_Content.html', 'w')
-create_file.write(str(results))
-create_file.close()
-
-# ! The range is always 1 + 7 = 8 this equal to the next field
-#The table can be read it by manual calculation, if you go to the table.html, the td where the data is storage the posistion is updated by 7
-# * With this code you can obtain the date of the game, the range is the next one 1, 8, 15
-table_1 = results[0].find_all('td', class_='zentriert')
-text = table_1[1].getText()
-
-#With this code we need to obtain the time
-# * the range for the time starts at 2, 9
-table_2 = results[0].find_all('td', class_='zentriert')
-text = table_2[2].getText()
-
-#With this code we can obtain the Home team
-# * IMPORTANT: To obtain the home team and the way team is necessary to check a different tag in the html file, so for this the range is different
-# TODO: Is necessary to do a parser, right now the obtained string is like this "UNAM Pumas (8. )", the string after the name of the team is not correct, I JUST NEED THE NAME OF THE TEAM
-# TODO create a function to remove that part is not necessary.
-table_3 = results[0].find_all('td', class_='no-border-links')
-text = table_3[0].getText()
-print(text)
