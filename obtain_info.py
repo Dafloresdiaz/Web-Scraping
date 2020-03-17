856cbd2ab60fbdddcc006e578ca9cd7913528f75
Dafloresdiaz <dafloresdiaz@gmail.com>
2020-03-16 19:30:44 -0600

    Obtain Date
    Obtain the date from each page and print the result in the console. This will be the same for each filed that i want to obtain or see

--- obtain_info.py
+++ obtain_info.py
@@ -14,10 +14,16 @@ class obtain_info_from_sections:
         #data is storage the posistion is updated by 7
         # * With this code you can obtain the date of the game, the range is the next one 1, 8, 15
         #TODO make a for to go to every date from the html page
-        for i in int(len(results)):
+        for i in range(int(len(results))):
             self.time = results[i].find_all('td', class_='zentriert')
-            self.text = self.time[x].getText()
-            # ! The range is always 1 + 7 = 8 this equal to the next field
-            x = x + 7
-            print(self.text)
-        
+            self.x = 1
+            for y in range(int(len(self.time))):
+                if self.x < int(len(self.time)):
+                    self.text = self.time[self.x].getText()
+                    print(self.text)
+                    # ! The range is always 1 + 7 = 8 this equal to the next field
+                    self.x = self.x + 7
+                else:
+                    break
+                
+            
