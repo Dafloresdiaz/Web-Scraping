ddb7c935463a401f3d41f0195e2f07ec399411d6
dafloresdiaz <dafloresdiaz@gmail.com>
2020-06-07 00:55:17 -0500

    Solve the issue for empty spaces
    solve the issues with the empty spaces in the database, I need to sovle the issue for a entry with a lot of spaces

--- created_queries.py
+++ created_queries.py
@@ -25,8 +25,9 @@ class create_queries:
             self.connection.commit()
 
     def insert_game_plan(self, planStrategy):
-        self.querie.execute("SELECT COUNT(*) FROM game_plan WHERE gamePlan = '%s'" % (planStrategy))
-        result = self.querie.fetchone()[0]
-        if result == 0:
-            self.querie.execute("INSERT INTO game_plan(gamePlan) VALUES('%s')" % (planStrategy))
-            self.connection.commit()
+        if len(planStrategy) > 1:
+            self.querie.execute("SELECT COUNT(*) FROM game_plan WHERE gamePlan = '%s'" % (planStrategy))
+            result = self.querie.fetchone()[0]
+            if result == 0:
+                self.querie.execute("INSERT INTO game_plan(gamePlan) VALUES('%s')" % (planStrategy))
+                self.connection.commit()
